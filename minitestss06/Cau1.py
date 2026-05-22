9ef65040a93e157b28042b7c3d5993606b43d722
ducmanht6-hub <ducmanht6@gmail.com>
2026-05-22 07:36:21 +0700

    Delete minitestss06 directory

--- minitestss06/Cau1.py
+++
@@ -1,7 +0,0 @@
-item_price = int(input('Nhập đơn giá sản phẩm của bạn: '))
-item_quantity = int(input('Nhập số lượng mua: '))
-
-total_price = item_price * item_quantity
-if total_price >= 1000000:
-    total_discount = total_price * 0.1
-    print(f'Giảm 10% tổng tiền, giá sau phải trả: {total_discount}')
--- minitestss06/Cau2.py
+++
@@ -1,9 +0,0 @@
-password = 123456
-for password_check in range(3, 0, -1):
-    input_password = int(input(f'Hãy nhập mật khẩu để đăng nhập (còn {password_check} lần): '))
-    if input_password == password:
-        print('Đăng nhập thành công!')
-        exit()
-    else:
-        print('Mật khẩu sai, vui lòng nhập lại!')
-print('Tài khoản đã bị khóa!')
